9aea1a5d8ee2b9a6370be87ac7434fdf9538c51d
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-09-23 17:19:29 +0200

    done

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,43 +3,41 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-    
-    <header>
+    <header class="header">
         <nav class="container">
-            <div >
-                <img 
-                    style="width : 200px ; height : auto"
-                    src="https://uploads-ssl.webflow.com/601d6f7d0b9c984f07bf10bc/62983fa8ef716259c397a57d_logo.svg">
-            </div>
-            <ul class="nav-routes routeStyle" >
-                <RouterLink to="/">Admin</RouterLink>
-                <RouterLink to="/client">Client</RouterLink>
-                <RouterLink to="/reservations">My Reservations</RouterLink>
+            <img style="width: 200px !important; height: auto !important;"
+                src="https://uploads-ssl.webflow.com/601d6f7d0b9c984f07bf10bc/62983fa8ef716259c397a57d_logo.svg">
+            <ul class="nav-routes" style="list-style: none; display: flex;">
+                <RouterLink to="/" exact active-class="active-route" class="routeStyle">
+                    Admin
+                </RouterLink>
+                <RouterLink to="/client" exact active-class="active-route" class="routeStyle">
+                    Client
+                </RouterLink>
+                <RouterLink to="/reservations" exact active-class="active-route" class="routeStyle">
+                    My Reservations
+                </RouterLink>
             </ul>
         </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
-header {
-    background-color: #45674a;
+.header {
+    // background: linear-gradient(to bottom, rgba(69, 103, 74, 0.9) 0%, rgba(138, 214, 151, 0.5) 100%);
 
+    background: linear-gradient(217deg, rgba(250, 255, 252, 0.8), rgba(255, 0, 0, 0) 70.71%),
+        linear-gradient(127deg, rgba(47, 245, 106, 0.8), rgba(0, 255, 0, 0) 90.71%),
+        linear-gradient(336deg, rgba(131, 255, 193, 0.8), rgba(0, 0, 255, 0) 90.71%);
+        
     nav {
         display: flex;
         align-items: center;
         padding: 25px 16px;
 
-        .branding {
-            display: flex;
-            align-items: center;
-            gap: 8px;
-
+        div {
             img {
                 max-width: 50px;
-            }
-
-            h1 {
-                font-size: 24px;
             }
         }
 
@@ -48,20 +46,26 @@ header {
             flex: 1;
             justify-content: flex-end;
             gap: 12px;
-            list-style: none;
-
-            a {
-                text-decoration: none;
-                color: inherit;
-            }
         }
 
-        .routeStyle{
-            padding-top : 15px;
-            color : white ; 
+        .routeStyle {
+            text-decoration: none;
+            color: black;
+            position: relative;
+            padding-top: 10px;
+            padding-left: 10px;
+
+            &.active-route {
+                &:after {
+                    content: "";
+                    position: absolute;
+                    left: 0;
+                    right: 0;
+                    bottom: -2px;
+                    border-bottom: 2px solid black;
+                }
+            }
         }
     }
 }
-
-
 </style>
